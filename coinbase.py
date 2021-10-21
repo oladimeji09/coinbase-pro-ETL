7fdfed0899788907822f3887a2f19c641124ed67
oladimeji09 <dimejiola@msn.com>
2021-10-21 18:34:45 +0000

    minor changes

--- coinbase.py
+++ coinbase.py
@@ -1,10 +1,13 @@
-import hmac, hashlib, time, requests, base64, pandas as pd, sys, env
+#@auto-fold regex /./
+import hmac, hashlib, time, requests, base64, pandas as pd, sys
 from requests.auth import AuthBase
+sys.path.insert(0,'C:/Finance/projects')
+import env,json
 
 items = []
 baseURL = 'https://api.pro.coinbase.com/'
-creds=(('api_key','secret_key', 'passphrase'),
-('api_key','secret_key','passphrase'))
+creds = json.load(open(r'C:\Finance\projects\personal\creds.json')).get('coinbase')
+base_creds = creds.get('portfolio_1')
 
 # Create custom authentication for Exchange
 class CoinbaseExchangeAuth(AuthBase):
@@ -32,30 +35,33 @@ class CoinbaseExchangeAuth(AuthBase):
 def get_profiles():
     """Return active profiles"""
     profiles =[]
-    for item in request('profiles'):
+    for item in request('profiles',base_creds.get('api_key'),
+                    base_creds.get('secret_key'), base_creds.get('passphrase')):
         if item['active'] == True:
              profiles.append([item['id'],item['name']])
     return profiles
 
-def request(end_point, a=creds[0][0],b=creds[0][1],c=creds[0][2]):
-    resp=requests.get(baseURL+end_point, auth=CoinbaseExchangeAuth(a,b,c))
+def request(end_point, api_key,secret_key,passphrase):
+    resp=requests.get(baseURL+end_point, auth=CoinbaseExchangeAuth(api_key,secret_key,passphrase))
     return resp.json()
 
 def get_ids():
     """Get a list of product ids"""
     ids = []
-    for i in list(request('accounts')):
+    for i in list(request('accounts',base_creds.get('api_key'),
+                    base_creds.get('secret_key'),base_creds.get('passphrase') )):
         ids.append(i['currency']+'-EUR')
         ids.append(i['currency']+'-GBP')
     return ids
 
 def get_fills():
     """Get a list of transactions"""
-    for i in creds: #loop through all profiles
-        for e in get_ids():   #loop through all product_ids
-            print("Geting data for {} using {} account.".format(e,i[2]))
+    for folio in creds: #loop through all portfolio
+        for product in get_ids():   #loop through all product_ids
+            print("Geting data for {} using {}.".format(product,folio))
             time.sleep(1)
-            for item in request('fills?product_id={}'.format(e),i[0],i[1],i[2]): #loop through all product_ids
+            for item in request('fills?product_id={}'.format(product),creds.get(folio).get('api_key')
+                                ,creds.get(folio).get('secret_key'),creds.get(folio).get('passphrase')): #
                 if 'message' not in item:
                     if item:
                         items.append(item)
@@ -63,15 +69,14 @@ def get_fills():
 
 def transform():
     """Transform data and export to google sheets"""
-    df= pd.DataFrame()
-    df = df.append(pd.io.json.json_normalize(get_fills()))
+    df = pd.json_normalize(get_fills())
     df[['ticker','currency']] = df.product_id.str.split('-',expand=True)
     for i in  get_profiles():
         df.loc[df.profile_id == i[0], ['profile_id']] = i[1]
     df['load_date']= pd.Timestamp.now().strftime('%Y-%m-%d  %H:%M:%S')
     df.created_at=pd.to_datetime(df.created_at).dt.strftime('%Y-%m-%d  %H:%M:%S')
     df = df.sort_values(['created_at'], ascending=True )
-    env.rep_data_sh(df,'google sheet id','Crypto: Data',df.shape[1])
+    env.rep_data_sh(df,'18HjRhb8maIt0ypGxSAmjz592_1oQZqEN0f915RlGsjw','Crypto: Data')
     return "Coinbase job completed successfully"
 
 results = transform()
